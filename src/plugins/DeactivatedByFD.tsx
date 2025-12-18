dd83fff02a89b333e9944226d1b969bf59babf6f
anirudhb <anirudhb@users.noreply.github.com>
2025-12-18 11:33:54 -0800

    DeactivatedByFD: just show deactivated accounts normally

--- src/plugins/DeactivatedByFD.tsx
+++ src/plugins/DeactivatedByFD.tsx
@@ -9,29 +9,13 @@ const init: RopePluginInit = (api) => {
       s2.members = Object.fromEntries(Object.entries(s2.members.__proto__)
         .map(([k, v]: [any, any]) => [k, {
           ...v,
-          deleted: v.enterprise_user.teams.length === 0 ? "by_fd" : v.deleted,
+          deleted: v.enterprise_user.teams.length === 0 ? true : v.deleted,
         }]));
     }
     return s2;
   });
 
-  const { SvgIcon } = api.slack;
-
-  const unpatchMemberProfileRestriction = api.react.patchComponentWithTester2(api.slack.MemberProfileRestriction, (props) => props.member?.deleted === "by_fd", MemberProfileRestriction => props => {
-    return <div className={`${props.className ?? ""} p-member_profile_restriction_deleted`}>
-      <div className="p-member_profile_restriction__svg_icon">
-        <SvgIcon inline={true} name="archive-filled" />
-      </div>
-      <span className="p-member_profile_restriction__text_deleted">
-        Deactivated by FD
-      </span>
-    </div>
-  });
-
-  /* popover isn't patchable because it doesn't give us enough info */
-
   return () => {
-    unpatchMemberProfileRestriction();
     unpatchMemberReduxPatch();
   };
 };
