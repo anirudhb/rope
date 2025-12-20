d9b176b4767d2ff8d9e5719a8a8cc6d0ea59cba2
anirudhb <anirudhb@users.noreply.github.com>
2025-12-20 00:00:31 -0800

    insane csp bypass

--- src/userscript.ts
+++ src/userscript.ts
@@ -1,5 +1,5 @@
 /* Userscript entry point */
-import "./main";
+//import "./main";
 //async function sleep(ms: number) {
 //  await new Promise(r => setTimeout(r, ms));
 //}
@@ -27,3 +27,42 @@ import "./main";
 //for (const k of globalThis_keys2)
 //  if (!globalThis_keys1.includes(k))
 //    unsafeWindow[k] = globalThis[k];
+
+async function main() {
+  /* HACK? try to override csp */
+  const parsedScripts = await (async function() {
+    window.stop();
+    const r = await fetch(location.href);
+    const t = await r.text();
+    const t2 = t.replace(/<meta http-equiv="Content-Security-Policy".*?>/g, "");
+    const p = new DOMParser();
+    const parsed = p.parseFromString(t2, "text/html");
+    const parsedScripts = Array.from(parsed.querySelectorAll("script")).map(s => {
+      return {
+        type: s.type,
+        src: s.src,
+        textContent: s.textContent,
+      };
+    });
+    console.log(t2);
+    document.documentElement.replaceWith(parsed.documentElement);
+    return parsedScripts;
+  })();
+
+  await import("./main");
+
+  // rerun scripts
+  document.querySelectorAll("script").forEach(s => s.parentNode.removeChild(s));
+  for (const s of parsedScripts) {
+    const scriptEl = document.createElement("script");
+    if (s.type)
+      scriptEl.type = s.type;
+    if (s.src)
+      scriptEl.src = s.src;
+    if (s.textContent)
+      scriptEl.textContent = s.textContent;
+    document.body.appendChild(scriptEl);
+  }
+}
+
+main();
