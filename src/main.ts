8785e6b120b9835f999d6945b638ab61e9b48df1
anirudhb <anirudhb@users.noreply.github.com>
2026-05-12 22:03:42 -0700

    first

--- src/main.ts
+++ src/main.ts
@@ -1,50 +1,109 @@
-import "./slack";
-import * as plugins from "./plugins";
-import * as taut from "./taut";
+import * as webpack from "jspatching/webpack";
+import * as react from "jspatching/react";
 
-import menuPlugin from "./plugins/menu";
-import InvisibleForward from "./plugins/InvisibleForward";
-import PrivateChannelMapper from "./plugins/PrivateChannelMapper";
-import DeactivatedByFD from "./plugins/DeactivatedByFD";
-//import MonacoMessageEditor from "./plugins/MonacoMessageEditor";
-import CodemirrorMessageEditor from "./plugins/CodemirrorMessageEditor";
+import * as patch from "./patch";
 
-import PrivateChannel from "./plugins/taut/PrivateChannel";
-import TautInvisibleForward from "./plugins/taut/InvisibleForward";
-import IdvStatus from "./plugins/taut/IdvStatus";
-import Oneko from "./plugins/taut/Oneko";
-import ShinigamiEyes from "./plugins/taut/ShinigamiEyes";
+/* Patch a simple module */
+const chunkName = "webpackChunkwebapp";
 
-/* initialize rope plugins */
-const ropePlugins: plugins.RopePlugin[] = [
-  menuPlugin,
-  InvisibleForward,
-  PrivateChannelMapper,
-  DeactivatedByFD,
-  //MonacoMessageEditor,
-  CodemirrorMessageEditor,
-];
+function createCachedMetadata() {
+  // Find the React import
+  const reactId = webpack.tryFindWebpackExportId(chunkName, m => m?.createElement);
+  if (!reactId) return null;
 
-for (const p of ropePlugins)
-  plugins.registerRopePlugin(p);
+  // Find the Legend react element
+  const legendId = webpack.tryFindWebpackExportId(chunkName, m => react.getComponentName(m) === "Legend");
+  if (!legendId) return null;
 
-// ensure menu is enabled
-plugins.setRopePluginEnabled(menuPlugin.id, true, false);
-
-/* initialize taut plugins */
-const tautPlugins: taut.TautPluginConstructor[] = [
-  // order probably matters, but shrug
-  PrivateChannel,
-  TautInvisibleForward,
-  IdvStatus,
-  Oneko,
-  ShinigamiEyes,
-];
-
-for (const pc of tautPlugins) {
-  const ropePlugin = taut.adaptTautPlugin(pc);
-  plugins.registerRopePlugin(ropePlugin);
+  return { reactId, legendId };
 }
 
-/* start plugins */
-plugins.startConfiguredRopePlugins();
+// Fetch cached metadata
+function getCachedMetadata() {
+  let d = localStorage.getItem("rope_cached_metadata");
+  if (d !== null)
+    return JSON.parse(d);
+  else
+    return null;
+}
+
+function createLegendPatch({ legendId, reactId }: {
+  legendId: webpack.WebpackExportId;
+  reactId: webpack.WebpackExportId;
+}): webpack._3type_WebpackPatch[] {
+  return patch.createAndConsolidatePatches([
+    {
+      debugName: "rope-legend",
+      exportId: legendId,
+      patch: (require, orig) => {
+        // Fetch original React
+        const React = webpack.requireWebpackExport(require, reactId);
+
+        return react.patchedComponent(orig, (props: any) => {
+          if (props.children === "Input options")
+            props = { ...props, children: "Input options (patched with Rope!)" };
+          return React.createElement(orig, props);
+        });
+      },
+    },
+  ]);
+  /*
+    {
+      debugName: "rope-log-react",
+      moduleId: reactId.moduleId.moduleId,
+      patch: (orig) => {
+        return function(module, exports, require) {
+          orig(module, exports, require);
+          console.log(module);
+          console.log(exports);
+        };
+      },
+    },
+  ];
+  */
+}
+
+function init() {
+  const d = getCachedMetadata();
+  if (d === null) {
+    globalThis.ropeCallback = function() {
+      // cache
+      let d2 = createCachedMetadata();
+      if (d2 === null) {
+        console.log(`[Rope] Failed to create cached metadata`);
+        return;
+      }
+
+      localStorage.setItem("rope_cached_metadata", JSON.stringify(d2));
+      console.log(`[Rope] Cached metadata created, will be used on next reload`);
+    };
+    console.log(`[Rope] No cached metadata, added callback on window`);
+
+    return;
+  }
+
+  console.log(`[Rope] Found cached metadata`);
+  console.log(d);
+  // Create patch
+  const legendPatches = createLegendPatch(d);
+  webpack._3type_hookWebpackChunkEarly(chunkName, [...legendPatches]);
+  console.log(`[Rope] Applied patches early`);
+}
+
+init();
+
+//import menuPlugin from "./plugins/menu";
+//
+///* Gather plugin metadata */
+//const ropePlugins: plugins.RopePlugin[] = [
+//  menuPlugin,
+//];
+//
+//for (const p of ropePlugins)
+//  plugins.registerRopePlugin(p);
+//
+//// ensure menu is enabled
+//plugins.setRopePluginEnabled(menuPlugin.id, true, false);
+//
+///* start plugins */
+//plugins.startConfiguredRopePlugins();
---
+++ src/patch.ts
@@ -0,0 +1,131 @@
+/** Handles patch transformations */
+
+import { sha256 } from "js-sha256";
+
+import { _3type_webpack_require_type, _3type_WebpackPatch, tryFindWebpackExportId, WebpackExportId, WebpackMatcher } from "jspatching/webpack";
+
+/**
+ * Converts a map of map of Webpack matchers into export IDs.
+ */
+export function lookupWebpackModulesBulk(chunkName: string, matchers: Record<string, Record<string, WebpackMatcher>>):
+  Record<string, Record<string, WebpackExportId | null>> {
+  // Avoids searching twice for the same matcher function
+  const foundModules = new Map();
+  const out = {};
+
+  for (const [k1, m] of Object.entries(matchers)) {
+    out[k1] = {};
+    for (const [k2, matcher] of Object.entries(m)) {
+      let id = foundModules.get(matcher);
+      if (typeof id === "undefined") {
+        id = tryFindWebpackExportId(chunkName, matcher);
+        foundModules.set(matcher, id);
+      }
+      out[k1][k2] = id;
+    }
+  }
+
+  return out;
+}
+
+/**
+ * Creates a hash of bulk Webpack matchers.
+ * The hash is based on the keys and stringified Functions.
+ * Therefore it is possible that this hash may change even if the actual matchers remain the same.
+ * The internal structure of this hash is subject to change.
+ */
+export function hashWebpackMatchers(matchers: Record<string, Record<string, WebpackMatcher>>): string {
+  const hasher = sha256.create();
+
+  for (const mk of Object.keys(matchers).toSorted()) {
+    const m = matchers[mk];
+    hasher.update(mk);
+    hasher.update("\0\0\0");
+    for (const ik of Object.keys(m).toSorted()) {
+      const i = m[ik];
+      hasher.update(ik);
+      hasher.update("\0");
+      hasher.update(i.toString());
+      hasher.update("\0\0");
+    }
+  }
+
+  return hasher.hex();
+}
+
+export type RopePatchedObject = {
+  exportId: WebpackExportId;
+  debugName: string;
+  patch: (require: _3type_webpack_require_type, orig: any) => any;
+};
+/**
+ * Creates and consolidates patches from a list of patched objects.
+ */
+export function createAndConsolidatePatches(patches: RopePatchedObject[]): _3type_WebpackPatch[] {
+  // Consolidate patches by module id
+  const patchesByModuleId: Record<string, {
+    root: RopePatchedObject[];
+    props: Record<string, RopePatchedObject[]>;
+  }> = {};
+
+  for (const p of patches) {
+    const m = patchesByModuleId[p.exportId.moduleId.moduleId] ??= { root: [], props: {} };
+    if (p.exportId.export === null) {
+      m.root.push(p);
+    } else {
+      (m.props[p.exportId.export] ??= []).push(p);
+    }
+  }
+
+  // For each module ID, create a patch
+  return Object.entries(patchesByModuleId).map(([moduleId, patches]) => ({
+    moduleId,
+    debugName: `rope-consolidated-${moduleId}`,
+    patch: (origModule) => function(module, exports, require) {
+      console.log(`[Rope] Running consolidated patch for module id ${moduleId}`);
+
+      const module2 = {};
+      const exports2 = {};
+      // Run the original module
+      origModule(module2, exports2, require);
+
+      // Copy properties
+      for (const [k, d] of Object.entries(Object.getOwnPropertyDescriptors(module2))) {
+        Object.defineProperty(module, k, { ...d, configurable: true });
+      }
+      for (const [k, d] of Object.entries(Object.getOwnPropertyDescriptors(exports2))) {
+        Object.defineProperty(exports, k, { ...d, configurable: true });
+      }
+
+      // Run root patches
+      if (module.exports) {
+        for (const rp of patches.root) {
+          console.log(`[Rope] Running root patch ${rp.debugName} for module id ${moduleId}`);
+          module.exports = rp.patch(require, module.exports);
+        }
+      }
+
+      // Run property patches
+      for (const [prop, patches2] of Object.entries(patches.props)) {
+        /* Determine how to get and set this prop
+         * Try getters on exports, then module.exports */
+        const getProp = Object.hasOwn(exports, prop)
+          ? () => exports[prop]
+          : Object.hasOwn(module.exports, prop)
+            ? () => module.exports[prop]
+            : null;
+        const setProp = Object.hasOwn(exports, prop)
+          ? (v: any) => Object.defineProperty(exports, prop, { get: () => v, configurable: true })
+          : Object.hasOwn(module.exports, prop)
+            ? (v: any) => module.exports[prop] = v
+            : null;
+        if (getProp === null || setProp === null) continue;
+
+        for (const pp of patches2) {
+          console.log(`[Rope] Running property patch ${pp.debugName} for property ${prop} of module ${moduleId}`);
+          setProp(pp.patch(require, getProp()));
+        }
+      }
+    },
+  }));
+}
